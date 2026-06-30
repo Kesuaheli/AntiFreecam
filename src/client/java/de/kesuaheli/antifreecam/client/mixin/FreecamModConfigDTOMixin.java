296ed4900ab3207f53f62711feaddf9797e805bd
Kesu <kesuaheligame@gmail.com>
2026-07-01 01:10:40 +0200

    feat: update to MC 1.21.11

--- src/client/java/de/kesuaheli/antifreecam/client/mixin/FreecamModConfigDTOMixin.java
+++ src/client/java/de/kesuaheli/antifreecam/client/mixin/FreecamModConfigDTOMixin.java
@@ -1,17 +1,17 @@
 package de.kesuaheli.antifreecam.client.mixin;
 
 import de.kesuaheli.antifreecam.client.integration.FreecamIntegration;
-import net.minecraft.block.Block;
-import net.xolt.freecam.config.CollisionBehavior;
+import net.minecraft.world.level.block.Block;
+import net.xolt.freecam.config.model.ModConfigDTO;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(CollisionBehavior.class)
-public class CollisionBehaviorMixin {
+@Mixin(ModConfigDTO.class)
+public class FreecamModConfigDTOMixin {
 
-	@Inject(method = "isIgnored", at = @At("HEAD"), remap = false, cancellable = true)
+	@Inject(method = "ignoreCollisionWith", at = @At("HEAD"), remap = false, cancellable = true)
 	private static void isIgnored(Block block, CallbackInfoReturnable<Boolean> cir) {
 		if (FreecamIntegration.forceCollision) {
 			cir.setReturnValue(false);
